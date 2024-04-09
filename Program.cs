1fcc44474d77dbcc283a5722a5618dbd88ba75f9
Russ Hammett <kritner@gmail.com>
2024-04-09 15:15:19 -0400

    Treatwarning as errors, and bad template message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddConsole());
+
+var host = builder.Build();
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("Hello world");
+
+var i = 5;
+logger.LogInformation("My value {i}}", i);
